f7a9475ff9fb474b4af2b0651b3981c8c241a176
TeoFerdian <teoderikus@accelbyte.net>
2019-08-22 14:01:45 +0700

    feat(UE4-SDK): update api-gateway session management

--- RunTests.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\Epic Games\UE_4.18\Engine\Binaries\Win64\UE4Editor-Cmd.exe" C:\Users\User\source\repos\accelbyte\justice-ue4-sdk\AccelByteUe4SdkDemo.uproject -Server -unattended -nopause -ExecCmds="Automation RunTests AccelByte.Tests.User.UpdateUserAccount;Quit" -log
-pause
