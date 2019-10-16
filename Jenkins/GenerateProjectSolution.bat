ceeb347ed95e503ac69ea92fa06e947e8c1034c0
Hendra Darwintha <hendra.darwintha@gmail.com>
2019-10-16 17:11:09 +0700

    feat(sdk): update to v2.1.0

--- Jenkins/GenerateProjectSolution.bat
+++
@@ -1,3 +0,0 @@
-::Re-generate project solution
-
-"%ENGINE_PATH%\Engine\Binaries\DotNet\UnrealBuildTool.exe" -projectfiles  -project="%WORKSPACE%\AccelByteUe4SdkDemo.uproject" -game -rocket -progress
